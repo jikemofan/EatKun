16483976808cf01015de529ad4af1844aad22915
jikemofan <59822311+jikemofan@users.noreply.github.com>
2022-07-13 10:04:04 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -124,7 +124,7 @@ function gameRestart() {
     _gameScore = 0;
     _gameOver = false;
     _gameStart = false;
-    _gameTimeNum = 25;
+    _gameTimeNum = 120;
     GameTimeLayer.innerHTML = creatTimeText(_gameTimeNum);
     countBlockSize();
     refreshGameLayer(GameLayer[0]);
